0b0432ea11aa2806a931df9a7763d925a307ac14
pixel <rider1999@list.ru>
2025-05-21 17:28:15 +0200

    исправил ошибку в homepage/views

--- homepage/views.py
+++ homepage/views.py
@@ -1,20 +1,54 @@
+from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
-from .models import StaticContent
-from games.models import Game
+from games.models import Game, GameSimilarity  # Импортируем модели из приложения games
 
 
 def home(request):
-    # Получаем статический контент
-    static_content = StaticContent.objects.all()
-    static_data = [
-        {'title': content.title, 'content': content.content, 'image': content.image.url if content.image else None} for
-        content in static_content]
+    # Получаем последние 5 игр по дате выпуска
+    latest_games = Game.objects.order_by('-release_date')[:5]
 
-    # Получаем динамический контент (например, последние игры)
-    latest_games = Game.objects.order_by('-created_at')[:5]
-    games_data = list(latest_games.values('id', 'title', 'description'))
+    # Получаем избранные игры
+    featured_games = Game.objects.filter(is_featured=True)
 
+    # Подготовка данных для передачи в JSON-ответ
+    def get_game_data(game):
+        system_requirements = game.system_requirements
+        similar_games = GameSimilarity.objects.filter(game=game).select_related('related_game')
+
+        similar_game_data = [
+            {
+                'id': sim.related_game.id,
+                'title': sim.related_game.title,
+                'similarity_percent': sim.similarity_percent
+            } for sim in similar_games
+        ]
+
+        return {
+            'id': game.id,
+            'title': game.title,
+            'slug': game.slug,
+            'slogan': game.slogan,
+            'short_description': game.short_description,
+            'release_date': game.release_date,
+            'media_cover_image': game.media_cover_image,
+            'metacritic_score': game.metacritic_score,
+            'ign_score': game.ign_score,
+            'system_requirements': {
+                'cpu': system_requirements.cpu if system_requirements else None,
+                'gpu': system_requirements.gpu if system_requirements else None,
+                'ram': system_requirements.ram if system_requirements else None,
+                'ssd': system_requirements.ssd if system_requirements else None,
+                'os': system_requirements.os if system_requirements else None,
+            },
+            'similar_games': similar_game_data,
+        }
+
+    # Сбор данных для последних и избранных игр
+    latest_games_data = [get_game_data(game) for game in latest_games]
+    featured_games_data = [get_game_data(game) for game in featured_games]
+
+    # Возвращаем JSON-ответ
     return JsonResponse({
-        'static_content': static_data,
-        'latest_games': games_data
+        'latest_games': latest_games_data,
+        'featured_games': featured_games_data,
     })
